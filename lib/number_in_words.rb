5cf3177f168e8c7c6a355fd6cc5373f946dc0c46
Sanjeev & mark rubenstein <student@epicodus.com>
2014-07-25 14:54:06 -0700

    Refactored code to clean up improve readability

--- lib/number_in_words.rb
+++ lib/number_in_words.rb
@@ -1,26 +1,14 @@
 def number_in_words(num)
-  num_word_list = {0 => "", 1 => "one", 2 => "two", 3 => "three",
-                      4 => "four", 5 => "five", 6 => "six", 7 => "seven",
-                      8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven",
-                      12 => "twelve", 13 => "thirteen", 14 => "fourteen",
-                      15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
-                      18 => "eighteen", 19 => "nineteen", 20 => "twenty",
-                      30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty",
-                      70 => "seventy", 80 => "eighty", 90 => "ninety", 100 => "hundred"
-                    };
+  num_word_list = {0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety", 100 => "hundred"};
   scale = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintrillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion', 'quindecillion', 'sexdecillion', 'septendecillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'unvigintillion', 'duovigintillion', 'trevigintillion', 'quattuortillion', 'quinvigintillion', 'sexvigintillion', 'septenvigintillion', 'octovigintillion', 'novemvigintillion', 'trigintillion', 'untrigintillion', 'duotrigintillion', 'trestrigintillion', 'quattuortrigintillion', 'quintrigintillion', 'sextrigintillion', 'septentrigintillion', 'octotrigintillion', 'novemtrigintillion', 'quadragintillion', 'unquadragintillion', 'duoquadragintillion', 'trequadragintillion', 'quattuorquadragintillion', 'quinquadragintillion', 'sesquadragintillion', 'septenquadragintillion', 'octoquadragintillion', 'novenquadragintillion', 'quinquagintillion']
 
   word_array = []
+
   i_scale = 0
 
   while !num.zero?
-    f = (num / 1000)
     hundreds_word_array = []
     num, num_sub = num.divmod(1000)
-    puts num
-    puts '----'
-    puts num_sub
-    puts '---------------'
     if num_sub >= 100 && num_sub <= 999
       hundreds, num_sub = num_sub.divmod(100)
       hundreds_word_array.push(num_word_list[hundreds])
@@ -29,6 +17,7 @@ def number_in_words(num)
         hundreds_word_array.push(scale[i_scale])
       end
     end
+
     if num_sub >= 20 && num_sub <= 99
       tens, num_sub  = num_sub.divmod(10)
       hundreds_word_array.push(num_word_list[tens * 10])
@@ -36,28 +25,15 @@ def number_in_words(num)
         hundreds_word_array.push(scale[i_scale])
       end
     end
+
     hundreds_word_array.push(num_word_list[num_sub])
     if !num_sub.zero?
       hundreds_word_array.push(scale[i_scale])
     end
+
     num_sub = 0
     i_scale = i_scale.next
     word_array.unshift(hundreds_word_array.join(' ').strip)
   end
-  word_array.join(' ').strip.sub(/( )+/, ' ')
+  word_array.join(' ').strip.sub(/ \s+/, ' ')
 end
-
-# puts number_in_words(256)
-# puts number_in_words(600)
-# puts number_in_words(1250)
-# puts number_in_words(1000)
-# puts number_in_words(10000)
-# puts number_in_words(111111)
-# puts number_in_words(100000)
-puts number_in_words(90000000)
-
-# irb(main):003:0> 100000.divmod(1000)
-# => [100, 0]
-
-# irb(main):004:0> 100.divmod(1000)
-# => [0, 100]
--- spec/number_in_words_spec.rb
+++ spec/number_in_words_spec.rb
@@ -17,4 +17,7 @@ describe :number_in_words do
   it 'returns word for a number greater 999 by using scale' do
     expect(number_in_words(1000481)).to eq('one million four hundred eighty one')
   end
+  it 'big number returns the written big number' do
+    expect(number_in_words(900004565464645645645645645645647465678584585678567858696979679678967000)).to eq('nine hundred duovigintillion four unvigintillion five hundred sixty five vigintillion four hundred sixty four novemdecillion six hundred forty five octodecillion six hundred forty five septendecillion six hundred forty five sexdecillion six hundred forty five quindecillion six hundred forty five quattuordecillion six hundred forty five tredecillion six hundred forty seven duodecillion four hundred sixty five undecillion six hundred seventy eight decillion five hundred eighty four nonillion five hundred eighty five octillion six hundred seventy eight septillion five hundred sixty seven sextillion eight hundred fifty eight quintrillion six hundred ninety six quadrillion nine hundred seventy nine trillion six hundred seventy nine billion six hundred seventy eight million nine hundred sixty seven thousand')
+  end
 end
